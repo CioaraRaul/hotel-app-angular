17c75a6ca2ffbf6b503b9b7e13fd426a9099d92b
CioaraRaul <cioararaul08@gmail.com>
2025-07-31 02:09:34 +0300

    working on reservation

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservation: Reservation[] = [];
 
-  constructor() { }
+  //crud
+
+  getReservations(): Reservation[] {
+    return this.reservation;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservation.find((res) => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservation.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservation.findIndex((res) => res.id === id);
+    this.reservation.splice(index, 1);
+  }
+
+  updateReservation(updateReservation: Reservation): void {
+    let index = this.reservation.findIndex(
+      (res) => res.id === updateReservation.id
+    );
+
+    this.reservation[index] = updateReservation;
+  }
 }
